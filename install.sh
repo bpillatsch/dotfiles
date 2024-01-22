ecb710fe9140cf15d745075560947f01062afb90
Brad Pillatsch <bpillatsch@gmail.com>
2024-01-22 15:47:00 -0800

    reorg

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e	#exit on error
+
+if ! command -v chezmoi; then
+  bin_dir="$HOME/.local/bin"
+  if ! command -v curl; then
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl installed." >&2
+    exit 1
+  fi
+fi
--- private_dot_config/private_fish/functions/ll.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /usr/share/fish/functions/ll.fish @ line 4
-function ll --wraps=ls --description 'List contents of directory using long format'
-    ls -lh --icons $argv
-end
