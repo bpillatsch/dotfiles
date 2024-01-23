1eba4b70c9f00c96c3308e37d17516c187b4f643
Brad Pillatsch <bpillatsch@gmail.com>
2024-01-22 16:46:57 -0800

    reorg

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 set -e	#exit on error
 
-if ! command -v chezmoi; then
+if ! command -v chezmoi &> /dev/null; then
   bin_dir="$HOME/.local/bin"
-  if command -v curl; then
+  if command -v curl &> /dev/null; then
     sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$bin_dir"
   else
     echo "To install chezmoi, you must have curl installed." >&2
